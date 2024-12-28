9a9cc0a977a5a327e69db4bc39b285d475e0887a
Binoculars7 <elitespot.com@gmail.com>
2024-12-28 08:25:51 +0100

    first commit

--- second.go
+++ second.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
-	fmt.Println(34)
+	//greetings := "Season greetings to you all"
+
+	// fmt.Println(strings.Contains(greetings, "you"))
+	// fmt.Println(strings.ReplaceAll(greetings, "greetings", "messages"))
+	// fmt.Println(strings.ToUpper(greetings))
+	//fmt.Println(strings.Index(greetings, "ll"))
+	//fmt.Println(strings.Split(greetings, " "))
+
+	ages := []int{20, 40, 50, 59, 29, 58, 70, 80, 90}
+
+	sort.Ints(ages)
+	fmt.Println(ages)
+
+	index := sort.SearchInts(ages, 59)
+	fmt.Println(index)
 }
